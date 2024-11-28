bc2956bd8ce5cc0e45abfe036c40ce04245d1ff1
tanvirhasan6 <tanvir.hasan6@gmail.com>
2024-11-28 14:47:59 +0600

    committed by tanvir for changing db calling

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const app = require("./app");
-const connection = require("./utils/ConnectOracle");
+const getDBConnection = require("./utils/ConnectOracle");
 
 
 // Oracle database connection
-connection.finally((err) => {
-  if (err) {
+getDBConnection()
+  .then(() => {
+    console.log("Connected to Oracle server");
+  })
+  .catch((err) => {
     console.error("Error connecting to Oracle:", err);
-    return;
-  }
-  console.log("Connected to Oracle server");
-});
+    process.exit(1); // Exit process if DB connection fails
+  })
+  .finally(() => {
+    console.log("Database connection attempt complete");
+  });
 
-// server port Connection
+// Server port connection
 const port = process.env.PORT || 5001;
 
 app.listen(port, () => {
